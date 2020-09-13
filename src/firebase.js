f76d8cad87075676304fe45e481e91fd6778a97c
paolo <gariboldipaolopaolo@gmail.com>
2020-09-14 00:33:32 +0200

    inizio sviluppo header

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCR05pAg-lriy0UDGDDqFn3RU_3txHghi0",
+    authDomain: "clone-b3fe6.firebaseapp.com",
+    databaseURL: "https://clone-b3fe6.firebaseio.com",
+    projectId: "clone-b3fe6",
+    storageBucket: "clone-b3fe6.appspot.com",
+    messagingSenderId: "282597213905",
+    appId: "1:282597213905:web:2d00a9ca3dded88ad227f0",
+    measurementId: "G-05TRHVV6V4"
+  };
